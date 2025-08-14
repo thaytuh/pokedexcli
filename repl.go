e9d7a20f506c3e33ca32c75d737c0b91ce5c9c80
thaytuh <htaylorkrass@gmail.com>
2025-08-14 15:44:42 -0700

    feat: added exit and help commands to the REPL loop

--- repl.go
+++ repl.go
@@ -18,6 +18,18 @@ func startRepl() {
 			continue
 		}
 
+		commandName := words[0]
+
+		if command, exists := getCommands()[commandName]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Printf("Unknown command\n")
+			continue
+		}
 	}
 }
 
@@ -25,4 +37,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name			string
+	description 	string
+	callback 		func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: 			"help",
+			description: 	"Displays a help message",
+			callback: 		commandHelp,
+		},
+		"exit": {
+			name:			"exit",
+			description:	"Exit the Pokedex",
+			callback: 		commandExit,
+		},
+	}
 }
