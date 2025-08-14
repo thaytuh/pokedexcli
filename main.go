d6c31fa43e800db2ab9703800a91b1c308f11453
thaytuh <htaylorkrass@gmail.com>
2025-08-14 13:53:49 -0700

    fix: moved startRepl and cleanInput functions into their own file to clean up main

--- main.go
+++ main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"os"
-	"fmt"
-	"strings"
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		rawText := scanner.Text()
-		cleanText := cleanInput(rawText)
-		fmt.Printf("Your command was: %v\n", cleanText[0])
-	}
-}
-
-func cleanInput(text string) []string {
-	cleanText := strings.Fields(text)
-	for i := range cleanText {
-		cleanText[i] = strings.ToLower(cleanText[i])
-	
-	}
-	return cleanText
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
